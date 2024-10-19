2ba2519cc7bf5e934a6b432a47fa423d169f9d26
Tony Dokyoon Kim <uhug@naver.com>
2024-10-19 23:45:24 +0900

    day1

--- presenter/src/main/java/space/doky/andadv1/presenter/MainActivity.kt
+++ presenter/src/main/java/space/doky/andadv1/presenter/MainActivity.kt
@@ -1,17 +1,38 @@
 package space.doky.andadv1.presenter
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import dagger.hilt.android.AndroidEntryPoint
 import space.doky.andadv1.presenter.databinding.ActivityMainBinding
+import space.doky.andadv1.presenter.viewmodel.MainViewModel
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        binding.view = this
+//        binding.viewModel = viewModel
+
+//        binding.textBox.addTextChangedListener {
+//            AppLog.e(TAG, "onCreate", "text changed: $it")
+//            viewModel.setSampleText(it.toString())
+//        }
+//
+//        lifecycleScope.launch {
+//            viewModel.sampleTextSharedFlow.collectLatest {
+//                AppLog.e(TAG, "onCreate", "shared flow: $it")
+//                binding.title.text = it
+//            }
+//        }
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
--- presenter/src/main/java/space/doky/andadv1/presenter/viewmodel/MainViewModel.kt
+++ presenter/src/main/java/space/doky/andadv1/presenter/viewmodel/MainViewModel.kt
@@ -1,6 +1,45 @@
 package space.doky.andadv1.presenter.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import space.doky.andadv1.domain.interactor.GetSampleTextCase
+import space.doky.andadv1.domain.interactor.SetSampleTextCase
+import space.doky.andadv1.util.di.IODispatcher
+import javax.inject.Inject
 
-class MainViewModel: ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    @IODispatcher private val ioDispatcher: CoroutineDispatcher,
+    private val getSampleTextCase: GetSampleTextCase,
+    private val setSampleTextCase: SetSampleTextCase,
+): ViewModel() {
+    private val _sampleTextSharedFlow = MutableSharedFlow<String>()
+    val sampleTextSharedFlow = _sampleTextSharedFlow
+
+    private val _sampleTextStateFlow = MutableStateFlow<String>("init")
+    val sampleTextStateFlow = _sampleTextStateFlow
+
+    init {
+        viewModelScope.launch {
+            getSampleTextCase().collectLatest {
+                _sampleTextStateFlow.emit(it)
+                _sampleTextSharedFlow.emit(it)
+            }
+        }
+    }
+
+    fun setSampleText(text: String) {
+        viewModelScope.launch {
+            withContext(ioDispatcher) {
+                setSampleTextCase(text)
+            }
+        }
+    }
 }
